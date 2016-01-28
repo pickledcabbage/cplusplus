cb03e4b85b2253de0562f7a23722b60387f8f2f1
pickledcabbage <dima.gutnik@gmail.com>
2016-01-28 11:38:55 -0800

    third commit

--- minesweeper/minesweeper/Source.cpp
+++ minesweeper/minesweeper/Source.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 void printBoard(int* arr, int height, int length)
 {
+	cout << endl;
 	cout << "   ";
 	for (int i = 0; i < length; i++)
 		cout << i;
@@ -27,9 +28,11 @@ void printBoard(int* arr, int height, int length)
 		}
 		cout << "|" << endl;
 	}
+	cout << endl;
 }
 void printBoard2(int* arr, int* revealed, int height, int length)
 {
+	cout << endl;
 	cout << "   ";
 	for (int i = 0; i < length; i++)
 		cout << i;
@@ -57,9 +60,11 @@ void printBoard2(int* arr, int* revealed, int height, int length)
 		}
 		cout << "|"<< endl;
 	}
+	cout << endl;
 }
 void revealBoard(int* arr, int* revealed, int height, int length, int x, int y)
 {
+	// SPOT (X,Y) IS TRYING TO BE EXPLORED, REVEAL BOARD BASED AROUND THAT.
 	if (arr[y*length + x] > 0)
 		revealed[y*length + x] = 1;
 	else
@@ -92,6 +97,7 @@ void revealBoard(int* arr, int* revealed, int height, int length, int x, int y)
 }
 bool check(int* arr, int height, int length, int bombs)
 {
+	// CHECKS NUMBER OF BOMBS POPPED
 	int counter = 0;
 	for (int i = 0; i < height*length; i++)
 	{
@@ -103,26 +109,9 @@ bool check(int* arr, int height, int length, int bombs)
 	}
 	return (counter == bombs);
 }
-int main()
+void getXandY(int& x, int& y, int HEIGHT, int LENGTH)
 {
-	const int HEIGHT = 9;
-	const int LENGTH = 9;
-	const int BOMBS = 10;
-	int board[HEIGHT][LENGTH];
-	int revealed[HEIGHT][LENGTH];
 
-	for (int y = 0; y < HEIGHT; y++)
-	{
-		for (int x = 0; x < LENGTH; x++)
-			board[y][x] = 0;
-	}
-	for (int y = 0; y < HEIGHT; y++)
-	{
-		for (int x = 0; x < LENGTH; x++)
-			revealed[y][x] = 0;
-	}
-	int x = -1;
-	int y = -1;
 	while (x < 0 || x > LENGTH - 1)
 	{
 		cout << "Enter the x coordinate where you want to explore (0 - " << LENGTH - 1 << "): ";
@@ -133,13 +122,33 @@ int main()
 		cout << "Enter the y coordinate where you want to explore (0 - " << HEIGHT - 1 << "): ";
 		cin >> y;
 	}
+}
+void makeNewGame(int* b, int* r, int HEIGHT, int LENGTH)
+{
+	for (int y = 0; y < HEIGHT; y++)
+	{
+		for (int x = 0; x < LENGTH; x++)
+			b[y*LENGTH+x] = 0;
+	}
+	for (int y = 0; y < HEIGHT; y++)
+	{
+		for (int x = 0; x < LENGTH; x++)
+			r[y*LENGTH+x] = 0;
+	}
+	printBoard2(b, r, HEIGHT, LENGTH);
+}
+void setUpBoard(int* board, int* revealed, int HEIGHT, int LENGTH, int BOMBS)
+{
+	int x = -1;
+	int y = -1;
+	getXandY(x, y, HEIGHT, LENGTH);
 	srand(time(NULL));
 	for (int i = 0; i < BOMBS; i++)
 	{
 		int xcor = rand() % LENGTH;
 		int ycor = rand() % HEIGHT;
-		if (board[ycor][xcor] == 0 && (ycor != y || xcor != x))
-			board[ycor][xcor] = 9;
+		if (board[ycor*LENGTH+xcor] == 0 && (ycor != y || xcor != x))
+			board[ycor*LENGTH+xcor] = 9;
 		else
 			i--;
 	}
@@ -149,47 +158,49 @@ int main()
 		for (int x = 0; x < LENGTH; x++)
 		{
 			int counter = 0;
-			if (board[y][x] != 9)
+			if (board[y*LENGTH+x] != 9)
 			{
-				if (x > 0 && board[y][x - 1] == 9)
+				if (x > 0 && board[y*LENGTH+(x - 1)] == 9)
 					counter++;
-				if (x < LENGTH - 1 && board[y][x + 1] == 9)
+				if (x < LENGTH - 1 && board[y*LENGTH+(x + 1)] == 9)
 					counter++;
-				if (y > 0 && board[y - 1][x] == 9)
+				if (y > 0 && board[(y - 1)*LENGTH+x] == 9)
 					counter++;
-				if (y < HEIGHT - 1 && board[y + 1][x] == 9)
+				if (y < HEIGHT - 1 && board[(y + 1)*LENGTH+x] == 9)
 					counter++;
-				if (x > 0 && y > 0 && board[y - 1][x - 1] == 9)
+				if (x > 0 && y > 0 && board[(y - 1)*LENGTH+(x - 1)] == 9)
 					counter++;
-				if (x < LENGTH - 1 && y < HEIGHT - 1 && board[y + 1][x + 1] == 9)
+				if (x < LENGTH - 1 && y < HEIGHT - 1 && board[(y + 1)*LENGTH + x + 1] == 9)
 					counter++;
-				if (x < LENGTH - 1 && y > 0 && board[y - 1][x + 1] == 9)
+				if (x < LENGTH - 1 && y > 0 && board[(y - 1)*LENGTH + x + 1] == 9)
 					counter++;
-				if (x > 0 && y < HEIGHT - 1 && board[y + 1][x - 1] == 9)
+				if (x > 0 && y < HEIGHT - 1 && board[(y + 1)*LENGTH + x - 1] == 9)
 					counter++;
-				board[y][x] = counter;
+				board[y*LENGTH+x] = counter;
 			}
 		}
 	}
+	revealBoard(&(board[0]), &(revealed[0]), HEIGHT, LENGTH, x, y);
+}
+int main()
+{
+	const int HEIGHT = 9;
+	const int LENGTH = 9;
+	const int BOMBS = 10;
+	int board[HEIGHT][LENGTH];
+	int revealed[HEIGHT][LENGTH];
 
-	revealBoard(&(board[0][0]), &(revealed[0][0]), HEIGHT, LENGTH, x, y);
+	makeNewGame(&board[0][0], &revealed[0][0], HEIGHT, LENGTH);
+	setUpBoard(&board[0][0], &revealed[0][0],HEIGHT, LENGTH, BOMBS);
+	
 	bool done = false;
 	bool win = false;
 	while (!done)
 	{
-		x= -1;
-		y = -1;
+		int x= -1;
+		int y = -1;
 		printBoard2(&(board[0][0]),&(revealed[0][0]), HEIGHT, LENGTH);
-		while (x < 0 || x > LENGTH-1)
-		{
-			cout << "Enter the x coordinate where you want to explore (0 - " << LENGTH - 1 << "): ";
-			cin >> x;
-		}
-		while (y < 0 || y > HEIGHT - 1)
-		{
-			cout << "Enter the y coordinate where you want to explore (0 - " << HEIGHT - 1 << "): ";
-			cin >> y;
-		}
+		getXandY(x, y, HEIGHT, LENGTH);
 		revealBoard(&(board[0][0]), &(revealed[0][0]), HEIGHT, LENGTH, x, y);
 		if (check(&(revealed[0][0]), HEIGHT, LENGTH, BOMBS))
 		{
